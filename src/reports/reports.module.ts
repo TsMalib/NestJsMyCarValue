20fedc05f399de5d8b40b10b13f56185cc02e0f8
Marcel "Malib" Beliš <malib.typicalstudio@gmail.com>
2024-12-04 21:08:20 +0100

    Add TypeORM integration and implement user creation
    This commit integrates TypeORM with SQLite into the application, establishing the use of entities for database interactions. A new endpoint for user signup is created, with validation in place. Furthermore, necessary dependencies for TypeORM and validation are added to the project, and test files are reorganized for better maintainability.

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { ReportsService } from './reports.service';
 import { ReportsController } from './reports.controller';
+import { Reports } from '../entities/reports.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Reports])],
   controllers: [ReportsController],
   providers: [ReportsService],
 })
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
-import { UsersService } from './users.service';
+import { Body, Controller, Post } from '@nestjs/common';
 
-@Controller('users')
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Controller('auth')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    console.log(body);
+  }
 }
